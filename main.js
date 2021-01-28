a6fa7bd1e80db07cf28b453ec172afb954f21a0f
parkjaehoon <yukihoon12@naver.com>
2021-01-28 16:48:20 +0900

    1월28일 목요일

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let http = require('http');
+let fs = require("fs");
 
-http.createServer(function(request, response){
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end("Hello World\n");
-}).listen(8081);
-console.log('Server running at http://127.0.0.1:8081');
+fs.readFile('input.txt', function(err, data){
+   if(err) return console.error(err);
+   console.log(data.toString());
+});
+
+console.log("Program has ended");
