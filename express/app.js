5da763c823118c36758351b45db74d3bb9fc5f30
parkjaehoon <yukihoon12@naver.com>
2021-02-05 17:01:23 +0900

    미들웨어 특성 활용하기

--- express/app.js
+++ express/app.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const path = require("path");
 
+dotenv.config();
+// app.use(bodyParser.raw());
 const app = express();
 app.set("port", process.env.PORT || 3000);
+app.use(morgan("dev")); //콘솔에 찍는거
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
 app.use("/", (req, res, next) => {
   console.log("모든 요청에 다 실행됩니다");
   next();
