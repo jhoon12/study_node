d9b8eede750d74adb81d9deb341dd86e7a981942
parkjaehoon <yukihoon12@naver.com>
2021-02-15 11:07:55 +0900

    동적 사이트 만들기

--- exprees-aMenBlog/index.js
+++ exprees-aMenBlog/index.js
@@ -1,12 +1,18 @@
-let express= require('express');
-let app = express();
+var express = require('express');
+var app = express();
 
-app.use(express.static(__dirname + '/public'));//HTTP method나 route에 상관없이 서버에 요청이 올 때마다 무조건 콜백함수가 실행됩니다.
-//원래는 app.use의 매개변수로 콜백을 받는데, express.static함수가 콜백함수를 리턴한다.
-// '현재_위치/public' route를 static폴더로 지정하라는 명령어
-//즉 '/'에 접속하면 '현재_위치/public'를, '/css'에 접속하면 '현재_위치/public/css'를 연결해 줍니다.
+app.set('view engine','ejs'); //ejs를 사용하기 위해 express의 view engine에 ejs를 set
+app.use(express.static(__dirname + '/public'));
 
-let port = 3000;
+app.get('/hello', function(req,res){ 
+  res.render('hello', {name:req.query.nameQuery});
+});
+
+app.get('/hello/:nameParam', function(req,res){ 
+  res.render('hello', {name:req.params.nameParam});
+});
+
+var port = 3000;
 app.listen(port, function(){
-    console.log('server on! http://localhost: ' + port);
-})
+  console.log('server on! http://localhost:'+port);
+});
