a2263f8d5388d4270204fba9e76bcf27e41d834a
parkjaehoon <yukihoon12@naver.com>
2021-03-14 23:52:43 +0900

    s

--- sns/routes/page.js
+++ sns/routes/page.js
@@ -13,6 +13,7 @@ router.use((req, res, next) => {
 router.get("/profile", (req, res) => {
   res.render("profile", { title: "내 정보 - NodeBird" });
 });
+
 router.get("/join", (req, res) => {
   res.render("join", { title: "회원가입 - NodeBird" });
 });
